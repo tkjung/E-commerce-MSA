599b60d7ccc521a539a27aadb9fe9a3c5dc77713
TK Jung <jtcjtc@naver.com>
2021-12-03 15:09:30 +0900

    2021-12-03:base

---
+++ Section 0-3/secondservice/src/main/java/com/example/secondservice/SecondserviceController.java
@@ -0,0 +1,23 @@
+package com.example.secondservice;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/second-service/")
+@Slf4j
+public class SecondserviceController {
+    @GetMapping("/welcome")
+    public String welcome() {
+        return "Welcome to the Second service";
+    }
+
+    @GetMapping("/message")
+    public String message(@RequestHeader("second-request") String header) {
+        log.info(header);
+        return "Hello World in Second Service";
+    }
+}
---
+++ userservice/src/main/java/com/example/userservice/controller/UserController.java
@@ -0,0 +1,15 @@
+package com.example.userservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class UserController {
+
+    @GetMapping("/health_check")
+    public String status() {
+        return "It's Working in User Service";
+    }
+}
