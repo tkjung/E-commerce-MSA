825adc1917e9cbf668f9413d30b63d0377b33d11
TK Jung <jtcjtc@naver.com>
2021-12-07 17:57:26 +0900

    2021-12-07:JPA-1

--- userservice/src/main/java/com/example/userservice/controller/UserController.java
+++ userservice/src/main/java/com/example/userservice/controller/UserController.java
@@ -1,23 +1,28 @@
 package com.example.userservice.controller;
 
+import com.example.userservice.dto.UserDto;
+import com.example.userservice.service.UserService;
 import com.example.userservice.vo.Greeting;
+import com.example.userservice.vo.RequestUser;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/")
 public class UserController {
     private Environment env;
+    private UserService userService;
 
     @Autowired
     private Greeting greeting;
 
     @Autowired
-    public UserController(Environment env) {
+    public UserController(Environment env, UserService userService) {
         this.env = env;
+        this.userService = userService;
     }
 
     @GetMapping("/health_check")
@@ -30,4 +35,16 @@ public class UserController {
         // return env.getProperty("greeting.message");
         return greeting.getMessage();
     }
+
+    @PostMapping("/users")
+    public String createUser(@RequestBody RequestUser user) {
+        ModelMapper mapper = new ModelMapper();
+        mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+
+        UserDto userDto = mapper.map(user, UserDto.class);
+        userService.createUser(userDto);
+
+        return "Create user method is called";
+    }
+
 }
