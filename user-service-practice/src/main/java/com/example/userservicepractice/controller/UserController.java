67ff7ca9a271ec9e2fac51b56d5cf02abd7828bb
TK Jung <jtcjtc@naver.com>
2021-12-13 17:47:41 +0900

    2021-12-13:modified2

--- user-service-practice/src/main/java/com/example/userservicepractice/controller/UserController.java
+++ user-service-practice/src/main/java/com/example/userservicepractice/controller/UserController.java
@@ -1,7 +1,11 @@
 package com.example.userservicepractice.controller;
 
+import com.example.userservicepractice.dto.UserDto;
+import com.example.userservicepractice.service.UserService;
 import com.example.userservicepractice.vo.Greeting;
 import com.example.userservicepractice.vo.RequestUser;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.*;
@@ -10,13 +14,15 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/")
 public class UserController {
     private Environment env;
+    private UserService userService;
 
     @Autowired
     private Greeting greeting;
 
     @Autowired
-    public UserController(Environment env) {
+    public UserController(Environment env, UserService userService) {
         this.env = env;
+        this.userService = userService;
     }
 
     @GetMapping("/health-check")
@@ -32,6 +38,12 @@ public class UserController {
 
     @PostMapping("/users")
     public String createUser(@RequestBody RequestUser user) {
+        ModelMapper mapper = new ModelMapper();
+        mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+
+        UserDto userDto = mapper.map(user, UserDto.class);
+        userService.createUser(userDto);
+
         return "Create user method is called";
     }
 }
--- user-service-practice/src/main/java/com/example/userservicepractice/service/UserServiceImpl.java
+++ user-service-practice/src/main/java/com/example/userservicepractice/service/UserServiceImpl.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
+
 @Service
 public class UserServiceImpl implements UserService {
     @Autowired
