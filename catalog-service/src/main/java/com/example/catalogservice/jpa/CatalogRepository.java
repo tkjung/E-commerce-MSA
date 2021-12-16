b5310e738fe194ff1587ce8f332b552007f07541
TK Jung <jtcjtc@naver.com>
2021-12-16 14:14:03 +0900

    2021-12-16:catalog-made-classes

---
+++ catalog-service/src/main/java/com/example/catalogservice/jpa/CatalogRepository.java
@@ -0,0 +1,7 @@
+package com.example.catalogservice.jpa;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CatalogRepository extends CrudRepository<CatalogEntity, Long> {
+    CatalogEntity findByProductId(String productId);  // 상품명으로 검색
+}
---
+++ catalog-service/src/main/java/com/example/catalogservice/vo/ResponseCatalog.java
@@ -0,0 +1,5 @@
+package com.example.catalogservice.vo;
+
+public class ResponseCatalog {
+
+}
