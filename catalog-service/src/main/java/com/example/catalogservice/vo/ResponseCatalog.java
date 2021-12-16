1b19110ae4265d272d352d755e3ad3e382ed77f4
TK Jung <jtcjtc@naver.com>
2021-12-16 16:50:58 +0900

    2021-12-16:catalog-sql-error-resolved

--- catalog-service/src/main/java/com/example/catalogservice/vo/ResponseCatalog.java
+++ catalog-service/src/main/java/com/example/catalogservice/vo/ResponseCatalog.java
@@ -1,5 +1,16 @@
 package com.example.catalogservice.vo;
 
-public class ResponseCatalog {
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
 
+import java.util.Date;
+
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL) // Null 값 데이터는 반환에서 목록에서 탈락시킴.
+public class ResponseCatalog {
+    private String productId;
+    private String productName;
+    private Integer unitPrice;
+    private Integer stock;
+    private Date createdAt;
 }
--- catalog-service/src/main/resources/data.sql
+++ catalog-service/src/main/resources/data.sql
@@ -1,8 +1,8 @@
 insert into catalog(product_id, product_name, stock, unit_price)
-    values('CATALOG-001', 'T-Shirts', 100, 1500)
+    values('CATALOG-001', 'T-Shirts', 100, 1500);
 
 insert into catalog(product_id, product_name, stock, unit_price)
-    values('CATALOG-002', 'Blue-Jeans', 110, 1000)
+    values('CATALOG-002', 'Blue-Jeans', 110, 1000);
 
 insert into catalog(product_id, product_name, stock, unit_price)
-    values('CATALOG-003', 'Gloves', 120, 2000)
+    values('CATALOG-003', 'Gloves', 120, 2000);
