a055f80373ed7bfd6282084f231a0dfa39e57ee2
TK Jung <jtcjtc@naver.com>
2021-12-23 18:04:28 +0900

    2021-12-23: Login-Start2

---
+++ userservice/src/main/java/com/example/userservice/security/AuthenticationFilter.java
@@ -0,0 +1,17 @@
+package com.example.userservice.security;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+        return super.attemptAuthentication(request, response);
+    }
+
+    // Success 구현
+}
