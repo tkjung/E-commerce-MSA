52ce74528cc0eea6e25e11320b6e0f7effa616c7
TK Jung <jtcjtc@naver.com>
2021-12-24 18:05:14 +0900

    2021-12-24: Authentication-Filter

--- userservice/src/main/java/com/example/userservice/security/AuthenticationFilter.java
+++ userservice/src/main/java/com/example/userservice/security/AuthenticationFilter.java
@@ -1,20 +1,35 @@
 package com.example.userservice.security;
 
+import com.example.userservice.vo.RequestLogin;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
+// 이메일, 패스워드를 받아서 로그인 인증 작업 처리.
 public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
-    @Override
-    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
-        return super.attemptAuthentication(request, response);
+    // InputStream 으로 데이터 받기 -> Request 변환 -> Email PW 추출 -> 인증 토큰 생성 -> 인증 처리
+    @Override // Override 단축키: Ctrl + O
+    public Authentication attemptAuthentication(HttpServletRequest request,
+                                                HttpServletResponse response) throws AuthenticationException {
+        try { // POST 메소드로 전달된 데이터는 Request 파라메터로 못 받으므로, InputStream 으로 받고 RequestLogin 으로 변환
+            RequestLogin creds = new ObjectMapper().readValue(request.getInputStream(), RequestLogin.class);
+
+        } catch(IOException e) { // IO 스트림 오류를 잡아주는 경우에 대해 명시해야 getInputStream() 오류가 안 뜬다.
+            throw new RuntimeException(e);
+        }
     }
 
-    // Success 구현
-    public int attempt() {
-        return 1;
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response,
+                                            FilterChain chain,
+                                            Authentication authResult) throws IOException, ServletException {
+        super.successfulAuthentication(request, response, chain, authResult);
     }
 }
