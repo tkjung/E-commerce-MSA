43580d2058e4875be2dff9407027694cd732b67a
TK Jung <jtcjtc@naver.com>
2021-12-28 18:03:19 +0900

    2021-12-28:API-Gateway

--- userservice/src/main/java/com/example/userservice/jpa/UserRepository.java
+++ userservice/src/main/java/com/example/userservice/jpa/UserRepository.java
@@ -4,4 +4,5 @@ import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepository extends CrudRepository<UserEntity, Long> {  // Long이 기본키인 Entity에 대해 CRUD 관련 기능 제공
     UserEntity findByUserId(String userId);
+    UserEntity findByEmail(String username);
 }
