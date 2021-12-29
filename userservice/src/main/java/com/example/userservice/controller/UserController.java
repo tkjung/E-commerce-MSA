5389334631d13b8ec27e1b833e133c05e244cc5b
TK Jung <jtcjtc@naver.com>
2021-12-29 13:01:45 +0900

    Update UserController.java

--- userservice/src/main/java/com/example/userservice/controller/UserController.java
+++ userservice/src/main/java/com/example/userservice/controller/UserController.java
@@ -17,8 +17,8 @@ import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 import java.util.List;
 
-@RestController // REST방식의 데이터 처리하기 위해 사용. REST란 1URI=1리소스를 대표
-@RequestMapping("/user-service/")
+@RestController // REST 방식의 데이터 처리하기 위해 사용. REST 란, 1URI = 1리소스를 대표
+@RequestMapping("/")
 public class UserController {
     private Environment env;
     private UserService userService;
