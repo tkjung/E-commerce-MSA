c7456d2d3cfbd0f4fc98d4c830d308c99e0744ad
TK Jung <jtcjtc@naver.com>
2022-01-06 13:49:13 +0900

    Update AuthenticationFilter.java

--- userservice/src/main/java/com/example/userservice/security/AuthenticationFilter.java
+++ userservice/src/main/java/com/example/userservice/security/AuthenticationFilter.java
@@ -12,7 +12,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-
+import java.util.ArrayList;
 
 public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
     @Override
