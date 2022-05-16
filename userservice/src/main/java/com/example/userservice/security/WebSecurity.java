f33121973b4576eeb6991a1c66fae3f9dc116ea1
TaeKwan Jung <50404450+TKJung@users.noreply.github.com>
2022-05-16 09:49:12 +0900

    2022-05-16
    update WebSecurity

--- userservice/src/main/java/com/example/userservice/security/WebSecurity.java
+++ userservice/src/main/java/com/example/userservice/security/WebSecurity.java
@@ -44,5 +44,10 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
         return authenticationFilter;
     }
 
- 
+    // SELECT pwd FROM users WHERE email=?it
+    // db_pwd(encrypted) == input_pwd(encrypted)
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userService).passwordEncoder(bCryptPasswordEncoder);
+    }
 }
