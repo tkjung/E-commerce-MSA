7f6b8a13e11bea57dbc77041fe553e4f01d52231
TaeKwan Jung <50404450+TKJung@users.noreply.github.com>
2022-05-17 23:10:38 +0900

    Update AuthenticationFilter.java

--- userservice/src/main/java/com/example/userservice/security/AuthenticationFilter.java
+++ userservice/src/main/java/com/example/userservice/security/AuthenticationFilter.java
@@ -36,11 +36,4 @@ public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
         }
     }
 
-    @Override
-    protected void successfulAuthentication(HttpServletRequest request,
-                                            HttpServletResponse response,
-                                            FilterChain chain,
-                                            Authentication authResult) throws IOException, ServletException {
-        log.debug(((User)authResult.getPrincipal()).getUsername());
-    }
 }
